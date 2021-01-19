0be1b4f7ba5cb3c551bde082b8814c12b48560b7
Alexander Horbatiuk <alexanderhorbatiuk@gmail.com>
2021-01-19 21:28:11 +0200

    message

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(() =>{
+
+    $(".slider_inner, .news_slider_inner").slick({
+        nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
+        infinite: false,
+    });
+
+    $('select').styler({
+        
+    })
+
+
+
+
+})
