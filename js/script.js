f57fa1529f40eb3f3c5edc2b67b9a76508209b28
Alexander Horbatiuk <alexanderhorbatiuk@gmail.com>
2021-01-20 14:35:42 +0200

    мобильная верся для главной страницы

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(() =>{
 
     $(".slider_inner, .news_slider_inner").slick({
-        nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
-        prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
-        infinite: false,
+      nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
+      prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
+      infinite: false,
+      dots: true,
     });
 
-    $('select').styler({
-        
+    $('select').styler();
+
+    $('.header_btn_menu').on('click', function () {
+        $('.menu ul').slideToggle()
     })
 
 
